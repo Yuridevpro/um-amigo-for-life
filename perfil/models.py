18a52900dc649e4b5b119e9c5371ae452da0a7c5
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2024-08-27 22:23:18 -0300

    mudancas 13

--- perfil/models.py
+++ perfil/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from storages.backends.s3boto3 import S3Boto3Storage
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
@@ -12,7 +12,7 @@ class UserProfile(models.Model):
     estado_id = models.IntegerField(blank=True, null=True)
     cidade_nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    foto_perfil = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    foto_perfil = models.ImageField(upload_to='profile_pics/', storage=S3Boto3Storage(), blank=True, null=True)
 
     def __str__(self):
         return f"{self.nome} {self.sobrenome}"
