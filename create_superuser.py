6a1ac5c01b642c9174b77215bd7bb273873924f5
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2024-08-28 20:20:59 -0300

    changes news 1

---
+++ create_superuser.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from dotenv import load_dotenv
+import os
+import sys
+
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Defina as credenciais do superusuário a partir das variáveis de ambiente
+SUPERUSER_USERNAME = os.getenv('SUPERUSER_USERNAME')
+SUPERUSER_EMAIL = os.getenv('SUPERUSER_EMAIL')
+SUPERUSER_PASSWORD = os.getenv('SUPERUSER_PASSWORD')
+
+def create_superuser():
+    User = get_user_model()
+    if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
+        User.objects.create_superuser(SUPERUSER_USERNAME, SUPERUSER_EMAIL, SUPERUSER_PASSWORD)
+        print("Superusuário criado com sucesso!")
+    else:
+        print("Superusuário já existe.")
+
+if __name__ == '__main__':
+    # Configure o ambiente Django
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adote.settings')
+
+    try:
+        # Configurar Django
+        import django
+        django.setup()
+        
+        # Criar superusuário
+        create_superuser()
+    except Exception as e:
+        print(f"Erro ao criar superusuário: {e}")
+        sys.exit(1)
