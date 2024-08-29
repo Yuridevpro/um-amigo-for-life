5fe8db162604c59af6e46ce840e7a5bd5f0cb41d
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2024-08-29 14:21:09 -0300

    novas 5

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('esqueceu_senha/', views.esqueceu_senha, name='esqueceu_senha'),
     path('criar_senha/<uidb64>/<token>/', views.criar_senha, name='criar_senha'),
     path('confirmar_email/<uidb64>/<token>/', views.confirmar_email, name='confirmar_email'),
-    path('auth/', include('social_django.urls', namespace='socialldede')),
+    path('', include('social_django.urls', namespace='socialldede')),
 
 ]
