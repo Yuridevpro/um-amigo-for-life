40702c224b4c741dcc866b9d83aa318140ef2fc9
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2024-08-30 18:29:15 -0300

    new 2

--- test_redis.py
+++ test_redis.py
@@ -1,7 +1,11 @@
 import redis
+import os
+
+# Obtém o URL do Redis a partir das variáveis de ambiente
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')  # Valor padrão para desenvolvimento local
 
 # Conectar ao Redis
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+r = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 # Testar a conexão
 try:
