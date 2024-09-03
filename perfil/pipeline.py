ee0411f9dc9fe87f301b48c0efe66abdac900cba
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2024-09-03 18:58:59 -0300

    pipeline 22

--- perfil/pipeline.py
+++ perfil/pipeline.py
@@ -1,14 +1,12 @@
 from django.contrib.auth import login
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.messages import constants
 from social_django.models import UserSocialAuth
-from social_core.exceptions import AuthAlreadyAssociated
 from .models import UserProfile
 
-# Função para criar ou atualizar o perfil do usuário
 def create_profile(strategy, details, user=None, *args, **kwargs):
     if user:
         try:
@@ -24,47 +22,60 @@ def create_profile(strategy, details, user=None, *args, **kwargs):
 
     return {'user': user}
 
-# Função para associar um e-mail a um usuário existente ou criar um novo usuário
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.urls import reverse
+from social_django.models import UserSocialAuth
+from .models import UserProfile
+
 def associate_by_email(backend, details, user=None, *args, **kwargs):
+    # Obtém o e-mail dos detalhes fornecidos pelo backend ou da sessão
     email = details.get('email') or backend.strategy.session_get('email')
-    
+
     if not email:
-        # Redireciona para a edição de perfil para preencher o e-mail
+        # Se o e-mail não estiver disponível, tente obtê-lo a partir do perfil do usuário
+        if user:
+            try:
+                user_profile = UserProfile.objects.get(user=user)
+                email = user_profile.email
+            except UserProfile.DoesNotExist:
+                pass
+
+    if not email:
+        # Se o e-mail ainda não estiver disponível, redireciona para o perfil para que o usuário possa fornecer o e-mail
         return {'user': None, 'redirect': reverse('editar_perfil')}
 
     try:
+        # Tenta obter um usuário existente com o e-mail fornecido
         existing_user = User.objects.get(email=email)
 
-        if user and user.pk != existing_user.pk:
-            # Se o e-mail já está em uso por outro usuário e é um usuário diferente
-            messages.add_message(backend.strategy.request, constants.ERROR, 'Este e-mail já está em uso por outro usuário.')
-            return render(backend.strategy.request, 'cadastro.html')
-
-        try:
-            # Verifica se o usuário já está associado a este provedor
-            social_auth = backend.strategy.storage.user.get_social_auth_for_user(existing_user)
-            if social_auth and social_auth.filter(provider=backend.name).exists():
-                # Se já existe uma associação com este provedor, loga o usuário
-                login(backend.strategy.request, existing_user, backend='social_core.backends.google.GoogleOAuth2')
-                return {'user': existing_user, 'redirect': reverse('home')}
-            else:
-                # Associa o novo provedor ao usuário existente
-                backend.strategy.storage.user.create_social_auth(user=existing_user, provider=backend.name, uid=details.get('uid'))
-                login(backend.strategy.request, existing_user, backend='social_core.backends.google.GoogleOAuth2')
-                return {'user': existing_user, 'redirect': reverse('home')}
-        
-        except AuthAlreadyAssociated:
-            # Captura a exceção caso o e-mail já esteja associado a outro provedor
-            messages.add_message(backend.strategy.request, constants.ERROR, 'Este e-mail já está associado a outro provedor.')
-            return render(backend.strategy.request, 'cadastro.html')
-
+        # Verifica se o usuário já está associado ao Facebook
+        social_auth = UserSocialAuth.objects.filter(user=existing_user, provider='facebook').first()
+        if social_auth:
+            # Se o usuário já estiver associado ao Facebook, faça login normalmente
+            login(backend.strategy.request, existing_user, backend='social_core.backends.facebook.FacebookOAuth2')
+            return {'user': existing_user, 'redirect': reverse('home')}
+        else:
+            # Se o usuário não estiver associado ao Facebook, crie a associação
+            backend.strategy.storage.user.create_social_auth(user=existing_user, provider='facebook', uid=details.get('uid'))
+            login(backend.strategy.request, existing_user, backend='social_core.backends.facebook.FacebookOAuth2')
+            return {'user': existing_user, 'redirect': reverse('editar_perfil')}
+    
     except User.DoesNotExist:
-        # Cria o usuário se não existir
-        user = User.objects.create_user(
-            username=email,  # Use o e-mail como username
-            email=email,
-            password=None
-        )
-        # Loga o usuário e redireciona para editar perfil
-        login(backend.strategy.request, user, backend='social_core.backends.google.GoogleOAuth2')
-        return {'user': user, 'redirect': reverse('editar_perfil')}
+        # Se o usuário não existir, crie um novo usuário
+        if email:
+            user = User.objects.create_user(
+                username=email,
+                email=email,
+                password=None
+            )
+            backend.strategy.storage.user.create_social_auth(user=user, provider='facebook', uid=details.get('uid'))
+            login(backend.strategy.request, user, backend='social_core.backends.facebook.FacebookOAuth2')
+            return {'user': user, 'redirect': reverse('editar_perfil')}
+        else:
+            # Se o e-mail não estiver disponível, redirecione para o perfil
+            return {'user': None, 'redirect': reverse('editar_perfil')}
+
+
+
